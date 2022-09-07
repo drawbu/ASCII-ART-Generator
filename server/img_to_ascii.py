8e6927fb24face34682e2e9c84d0a82d571dff0d
sigmanificient <edhyjox@gmail.com>
2022-09-07 20:17:59 +0200

    :memo: Added screenshot to readme

--- server/img_to_ascii.py
+++ server/img_to_ascii.py
@@ -11,7 +11,7 @@ IMG_SIZE = 100
 
 def main():
     # img = open_image(image_selector())
-    img = resize_img(open_image("images/urss.jpg"))
+    img = resize_img(open_image("../assets/drawbu.jpg"))
     img = ascii_to_img(img_to_ascii(img), img)
     img.show()
     print(encode_img(img))
