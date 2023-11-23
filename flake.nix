d1144718a01df3d28f898f3d459b0eaf61cb05c9
Sigmanificient <edhyjox@gmail.com>
2023-11-23 16:40:27 +0100

    :recycle: Add other systems, shell & formatter

--- flake.nix
+++ flake.nix
@@ -1,68 +1,97 @@
 {
-  outputs = { self, nixpkgs }: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    packages.${system} = rec {
-      default = client;
-      client = pkgs.buildNpmPackage {
-        name = "ascii-client";
-        version = "1.0.0";
+  outputs = { self, nixpkgs, }:
+    let
+      forAllSystems = function:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "aarch64-linux"
+          "x86_64-darwin"
+          "aarch64-darwin"
+        ]
+          (system: function nixpkgs.legacyPackages.${system});
 
-        src = ./client;
-        npmDepsHash = "sha256-wOF13k4rqv7VMaH7YLsO821cqXJ4+cFk/PQrQJRQ0Rg";
-
-        builtinsInputs = with pkgs; [ nodejs_20 ];
-
-        installPhase = ''
-          runHook preInstall
-
-          mkdir -p $out
-          cp -r public $out/
-
-          runHook postInstall
-        '';
+      py = pkgs: rec {
+        deps = p: with p; [ numpy opencv4 pillow ] ++ [ flask gunicorn ];
+        env = pkgs.python3.withPackages deps;
       };
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default =
+          let
+            pyenv = (py pkgs).env;
+            node = pkgs.nodejs_20;
+          in
+          pkgs.mkShell {
+            buildIputs = [ pyenv node ];
 
-      server = let
-        pyenv = pkgs.python3.withPackages (p: with p; [
-            numpy flask opencv4 pillow gunicorn
-        ]);
-      in pkgs.stdenvNoCC.mkDerivation {
-        name = "ascii-server";
-        version = "1.0.0";
+            shellHook = ''
+              echo "python env path -> ${pyenv}"
+              echo "node env path -> ${node}"
+            '';
+          };
+      });
 
-        src = pkgs.lib.cleanSource ./.;
+      formatter = forAllSystems (pkgs: pkgs.nixpkgs-fmt);
+      packages = forAllSystems (pkgs: rec {
+        default = server;
 
-        buildPhase = ''
-          runHook preBuild
+        client = pkgs.buildNpmPackage {
+          name = "ascii-client";
+          version = "1.0.0";
 
-          cat <<EOF > runner_script
-          #!${pkgs.runtimeShell}
+          src = ./client;
+          npmDepsHash = "sha256-wOF13k4rqv7VMaH7YLsO821cqXJ4+cFk/PQrQJRQ0Rg";
 
-          ${pyenv}/bin/gunicorn --workers 3 --bind \$1 -m 007 wsgi:app
-          EOF
+          builtinsInputs = with pkgs; [ nodejs_20 ];
 
-          runHook postBuild
-        '';
+          installPhase = ''
+            runHook preInstall
 
-        installPhase = ''
-          runHook preInstall
+            mkdir -p $out
+            cp -r public $out/
 
-          mkdir -p $out
+            runHook postInstall
+          '';
+        };
 
-          cp app.py $out
-          cp -r img_tools $out
+        server =
+          let pyenv = (py pkgs).env;
+          in pkgs.stdenvNoCC.mkDerivation {
+            name = "ascii-server";
+            version = "1.0.0";
 
-          cp -r ${client}/public $out
+            src = pkgs.lib.cleanSource ./.;
 
-          mkdir -p $out/bin
-          cp runner_script $out/bin/server
-          chmod +x $out/bin/server
+            buildPhase = ''
+              runHook preBuild
 
-          runHook postInstall
-        '';
-      };
+              cat <<EOF > runner_script
+              #!${pkgs.runtimeShell}
+
+              ${pyenv}/bin/gunicorn --workers 3 --bind unix:\$1 -m 007 wsgi:app
+              EOF
+
+              runHook postBuild
+            '';
+
+            installPhase = ''
+              runHook preInstall
+
+              mkdir -p $out
+
+              cp app.py $out
+              cp -r img_tools $out
+
+              cp -r ${client}/public $out
+
+              mkdir -p $out/bin
+              cp runner_script $out/bin/ascii-server
+              chmod +x $out/bin/ascii-server
+
+              runHook postInstall
+            '';
+          };
+      });
     };
-  };
 }
